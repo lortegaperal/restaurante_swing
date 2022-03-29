4dd50fd14ac33460f89c693b8f4f740b9db2f623
lortegaperal <lortegaperal@safareyes.es>
2022-03-29 20:36:50 +0200

    ventanas

--- src/main/java/pantalla/App.java
+++ src/main/java/pantalla/App.java
@@ -1,19 +1,43 @@
 package pantalla;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class App extends JFrame {
 
     public App(){
-        JPanel panel = new JPanel();
-        panel.setSize(300, 300);
+
+        JPanel panel_principal = new JPanel();
+        panel_principal.setSize(new Dimension(500, 500));
+        panel_principal.setBackground(Color.RED);
+        panel_principal.setOpaque(true);
+
+        JButton camarero = new JButton();
+        JButton cocinero = new JButton();
+
+        camarero.setText("Camarero");
+        cocinero.setText("Cocinero");
+
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.setBorder(new EmptyBorder(100, 100, 100, 100));
+        panel.setOpaque(true);
+        panel.setSize(new Dimension(300, 300));
         panel.setBackground(Color.BLUE);
+        camarero.setSize(new Dimension(100, 100));
+        cocinero.setSize(new Dimension(100, 100));
+        panel.add(camarero, BorderLayout.WEST);
+        panel.add(cocinero, BorderLayout.EAST);
+        panel_principal.add(panel, BorderLayout.CENTER);
+        panel_principal.add(panel);
 
         setContentPane(panel);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(new Dimension(500,500));
+
+
+
 
     }
 
