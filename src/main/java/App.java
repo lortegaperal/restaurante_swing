7586f755efdf7b47c1ff287114e2d201a56d62d6
lortegaperal <lortegaperal@safareyes.es>
2022-03-29 19:55:05 +0200

    creacion de clases y carpetas

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,12 @@
-public class App {
+import javax.swing.*;
+import java.awt.*;
+
+public class App extends JFrame {
+
+    public App(){
+        JPanel panel = new JPanel();
+        panel.setSize(300, 300);
+        panel.setBackground(Color.BLUE);
+    }
 
 }
